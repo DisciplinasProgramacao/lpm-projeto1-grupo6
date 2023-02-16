99c265a8981313bdd24c2b87b72d9aae07f8553d
Pedro Henrique Pessoa Cruz <1348623@sga.pucminas.br>
2023-02-15 21:46:52 -0300

    escrevendo testes java

---
+++ codigo/GestaoDeEstoque/src/ProdutoTeste.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import org.junit.jupiter.api.Test;
+
+public class ProdutoTeste {
+    @Test
+    public void descricaoNaoPodeConterMenosDe3Caracteres()
+    {
+        assertThrowsExactly(Exception.class, () -> new Produto("a", 20, 33));
+    }
+
+    @Test
+    public void precoCustoNaoPodeSerMenorOuIgualAZero()
+    {
+        assertThrowsExactly(Exception.class, () -> new Produto("aaa", 0, 33));
+    }
+
+    @Test
+    public void margemLucroNaoPodeSerMenorQue30()
+    {
+        assertThrowsExactly(Exception.class, () -> new Produto("aaa", 0, 29));
+    }
+
+    @Test
+    public void margemLucroNaoPodeSerMaiorQue80()
+    {
+        assertThrowsExactly(Exception.class, () -> new Produto("aaa", 31, 81));
+    }
+}
