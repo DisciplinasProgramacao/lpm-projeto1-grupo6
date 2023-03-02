840ed6188322647841081742b699c752faca93b4
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-03-02 15:01:29 -0300

    adicionando javadoc nos testes e montando novos testes

--- codigo/GestaoDeEstoque/src/ProdutoTeste.java
+++ codigo/GestaoDeEstoque/src/ProdutoTeste.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import java.security.InvalidParameterException;
@@ -5,23 +6,58 @@ import java.security.InvalidParameterException;
 import org.junit.jupiter.api.Test;
 
 public class ProdutoTeste {
+
+    Produto produtoTeste = new Produto("teste para fluxo perfeito", 100, 30);
+
+    /**
+     * Teste em prol de verificar se a descrição é aceita com MENOS de 3 caracteres.
+     */
     @Test
     public void descricaoNaoPodeConterMenosDe3Caracteres() {
-        assertThrowsExactly(Exception.class, () -> new Produto("a", 20, 33));
+        assertThrowsExactly(InvalidParameterException.class, () -> new Produto("12", 20, 33));
     }
 
+    /**
+     * Teste para verificar se PRECO CUSTO nao aceita entrada MENOR ou IGUAL a ZERO.
+     */
     @Test
     public void precoCustoNaoPodeSerMenorOuIgualAZero() {
-        assertThrowsExactly(Exception.class, () -> new Produto("aaa", 0, 33));
+        assertThrowsExactly(InvalidParameterException.class,
+                () -> new Produto("Descrição básica de um produto", 0, 33));
     }
 
+    /**
+     * Teste para verificar se a MARGEM DE LUCRO nao aceita entrada MENOR que 30.
+     */
     @Test
     public void margemLucroNaoPodeSerMenorQue30() {
-        assertThrowsExactly(Exception.class, () -> new Produto("aaa", 0, 29));
+        assertThrowsExactly(InvalidParameterException.class,
+                () -> new Produto("Descrição básica de um produto", 0, 10));
     }
 
+    /**
+     * Teste para verificar se a MARGEM DE LUCRO nao aceita entrada MAIOR que 80.
+     */
     @Test
     public void margemLucroNaoPodeSerMaiorQue80() {
-        assertThrowsExactly(Exception.class, () -> new Produto("aaa", 31, 81));
+        assertThrowsExactly(InvalidParameterException.class,
+                () -> new Produto("Descrição básica de um produto", 31, 100));
     }
+
+    /**
+     * Teste para verificação para cálculo de valor de venda.
+     */
+    @Test
+    public void retornarCalculoDeValordeVenda() {
+        assertEquals(100, produtoTeste.getPrecoVenda());
+    }
+
+    /**
+     * Verificando se o preço de custo é igual ao inserido quando objeto criado.
+     */
+    @Test
+    public void precoDeCustoDeveSerIGualAoInserido() {
+        assertEquals(100, produtoTeste.getPrecoCusto());
+    }
+
 }
