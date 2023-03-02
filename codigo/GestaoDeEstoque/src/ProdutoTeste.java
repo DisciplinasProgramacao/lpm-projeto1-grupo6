f22d9e4f08a95307b3bf3dba8512183049e08628
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-03-01 21:27:53 -0300

    adicionando o plano de testes

--- codigo/GestaoDeEstoque/src/ProdutoTeste.java
+++ codigo/GestaoDeEstoque/src/ProdutoTeste.java
@@ -4,26 +4,22 @@ import org.junit.jupiter.api.Test;
 
 public class ProdutoTeste {
     @Test
-    public void descricaoNaoPodeConterMenosDe3Caracteres()
-    {
+    public void descricaoNaoPodeConterMenosDe3Caracteres() {
         assertThrowsExactly(Exception.class, () -> new Produto("a", 20, 33));
     }
 
     @Test
-    public void precoCustoNaoPodeSerMenorOuIgualAZero()
-    {
+    public void precoCustoNaoPodeSerMenorOuIgualAZero() {
         assertThrowsExactly(Exception.class, () -> new Produto("aaa", 0, 33));
     }
 
     @Test
-    public void margemLucroNaoPodeSerMenorQue30()
-    {
+    public void margemLucroNaoPodeSerMenorQue30() {
         assertThrowsExactly(Exception.class, () -> new Produto("aaa", 0, 29));
     }
 
     @Test
-    public void margemLucroNaoPodeSerMaiorQue80()
-    {
+    public void margemLucroNaoPodeSerMaiorQue80() {
         assertThrowsExactly(Exception.class, () -> new Produto("aaa", 31, 81));
     }
 }
