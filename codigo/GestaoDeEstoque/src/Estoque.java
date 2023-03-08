d7e507bb098a279413e25aed13968710f70a42c7
Paulo Roncarati <roncarati1209@gmail.com>
2023-03-08 01:01:26 -0300

    Add construtor Estoque #20

--- codigo/GestaoDeEstoque/src/Estoque.java
+++ codigo/GestaoDeEstoque/src/Estoque.java
@@ -1,6 +1,13 @@
-public class Estoque {
+import java.security.InvalidParameterException;
 
-	public Estoque(int i) {
+public class Estoque {
+	Produto[] produtos;
+
+	public Estoque(int tamanho) {
+		if (tamanho < 1)
+			throw new InvalidParameterException("Erro ao criar estoque: O estoque deve ter pelo menos 1 produto");
+
+		produtos = new Produto[tamanho];
 	}
 
 }
