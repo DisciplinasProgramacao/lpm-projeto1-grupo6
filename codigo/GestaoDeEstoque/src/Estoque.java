a75cbc91acf65e4e37205acb047e49a2e30011ea
Paulo Roncarati <roncarati1209@gmail.com>
2023-03-08 09:08:47 -0300

    Add teste de reposicao de produto #20

--- codigo/GestaoDeEstoque/src/Estoque.java
+++ codigo/GestaoDeEstoque/src/Estoque.java
@@ -2,12 +2,38 @@ import java.security.InvalidParameterException;
 
 public class Estoque {
 	Produto[] produtos;
+	int tamanho;
 
 	public Estoque(int tamanho) {
 		if (tamanho < 1)
 			throw new InvalidParameterException("Erro ao criar estoque: O estoque deve ter pelo menos 1 produto");
 
 		produtos = new Produto[tamanho];
+		tamanho = 0;
 	}
 
+	public boolean estaVazio() {
+		return tamanho == 0;
+	}
+
+	public boolean estaCheio() {
+		return tamanho > produtos.length;
+	}
+
+	public int indexProduto(Produto produto) {
+		if (!estaVazio())
+			for (int i = 0; i < tamanho; i++)
+				if (produto.getId() == produtos[i].getId())
+					return i;
+		return -1;
+	}
+
+	public void repor(Produto produtoA, int quantidade) {
+		if (estaVazio()) {
+			produtos[0] = produtoA;
+		} else {
+			produtos[tamanho] = produtoA;
+		}
+		tamanho++;
+	}
 }
--- codigo/GestaoDeEstoque/src/EstoqueTeste.java
+++ codigo/GestaoDeEstoque/src/EstoqueTeste.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import java.security.InvalidParameterException;
@@ -10,4 +11,16 @@ public class EstoqueTeste {
 		assertThrowsExactly(InvalidParameterException.class, () -> new Estoque(0));
 	}
 
+	@Test
+	public void adicionarNovoProdutoCasoEleNaoEstejaNoEstoqueAoFazerReposicao() {
+		Estoque estoque = new Estoque(2);
+		Produto produtoA = new Produto("Banana", 10, 50);
+		Produto produtoB = new Produto("Maçã", 20, 30);
+		assertEquals(-1, estoque.indexProduto(produtoA));
+		assertEquals(-1, estoque.indexProduto(produtoB));
+		estoque.repor(produtoA, 10);
+		estoque.repor(produtoB, 10);
+		assertEquals(0, estoque.indexProduto(produtoA));
+		assertEquals(1, estoque.indexProduto(produtoB));
+	}
 }
