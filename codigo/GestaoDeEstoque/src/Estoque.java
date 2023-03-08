41d0375f3e83e9e351a7c4579f39d581cb915951
Paulo Roncarati <roncarati1209@gmail.com>
2023-03-08 00:56:29 -0300

    Add estoqueDeveConterPeloMenos1Produto

--- codigo/GestaoDeEstoque/src/Estoque.java
+++ codigo/GestaoDeEstoque/src/Estoque.java
@@ -1,3 +1,6 @@
 public class Estoque {
-	
+
+	public Estoque(int i) {
+	}
+
 }
--- codigo/GestaoDeEstoque/src/EstoqueTeste.java
+++ codigo/GestaoDeEstoque/src/EstoqueTeste.java
@@ -1,3 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import java.security.InvalidParameterException;
+
+import org.junit.Test;
+
 public class EstoqueTeste {
+	@Test
+	public void estoqueDeveConterPeloMenos1Produto() {
+		assertThrowsExactly(InvalidParameterException.class, () -> new Estoque(0));
+	}
 
 }
